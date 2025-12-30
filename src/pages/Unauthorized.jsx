295626b9ea1c81a74d09ad3ca7746bf0919dca80
manaluzufrin <manaluzufrin@yahoo.co.id>
2025-12-30 13:05:23 +0700

    update UnAuthorized page

--- src/pages/Unauthorized.jsx
+++ src/pages/Unauthorized.jsx
@@ -3,22 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function Unauthorized() {
   return (
-    <div className="container py-4">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-8 col-lg-6">
+    <div className="container py-5">
           <div className="card shadow-sm rounded-4">
             <div className="card-body p-4">
-              <h4 className="mb-2"><span class="text-danger">401</span> | Unauthorized</h4>
-              <p className="text-muted mb-4">
-                Akses di tolak. Anda tidak memiliki izin untuk membuka halaman ini.
-              </p>
-              <Link className="btn btn-dark" to="/contacts">
-                Kembali ke Contacts
+              <h4><span class="text-danger">401</span> | UnAuthorized</h4>
+              <p className="text-muted mb-3">Kamu tidak memiliki otoritas untuk mengakses halaman ini.</p>
+              <Link to="/contacts" className="btn btn-dark">
+                <i class="fa-solid fa-address-book me-2" /> Kembali ke Contacts
               </Link>
             </div>
           </div>
         </div>
-      </div>
-    </div>
   );
 }
